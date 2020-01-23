decb00624f48d47a5846d0dc6df169ee93311114
Nicholas Roberto <njr444@gmail.com>
2020-01-23 10:09:04 -0800

    created render and shopping cart functions to generate table programmatically.

--- shopping-cart/render-line-items.js
+++ shopping-cart/render-line-items.js
@@ -1,9 +1,16 @@
-<thead>
+//import cart from './data/cart.js';
+import coffee from './data/coffee.js';
+import { findById } from './common/utils.js';
+import { calcLineItem } from '../common/utils.js';
+
+
+{/* <thead>
     <tr>
       <th>Drink</th>
       <th>Qty</th> 
       <th>Price</th>
       <th>Total</th>
+      </tr>
 </thead>
 <tbody>
     <tr>
@@ -35,4 +42,53 @@
 </tfoot>
 <table>
 <button>Place Order</button>
- 
+  */}
+
+
+
+
+
+function renderTable(cartItem) {
+    const item = findById(cartItem.id, coffee);
+    const tabRow = document.createElement('tr');
+  
+    const tabData = document.createElement('td');
+    tabData.textContent = item.name;
+    tabRow.appendChild(tabData);
+
+    const tablePrice = document.createElement('td');
+    tablePrice.textContent = item.price;
+    tabRow.appendChild(tablePrice);
+
+    const tabTotal = document.createElement('td');
+    tabTotal.textContent = calcLineItem(cartItem.quantity, item.price);
+    tabRow.appendChild(tabTotal);
+    
+    return tabRow;
+    // const coffeeP = document.createElement('p');
+    // coffeeP.className = 'price';
+
+    // const usd = '$' + someTable.price.toFixed(2);
+    // coffeeP.textContent = usd;
+
+    // // const button = document.createElement('button');
+    // // button.textContent = 'Place Order';
+    // // button.value = coffee.id;
+    // // coffeeP.appendChild(button);
+
+
+}
+
+export default renderTable;
+
+
+
+// import coffee from '../data/coffee.js';
+// import renderTable from './render-line-items.js';
+
+// const tableBody = document.getElementById('table-generate');
+
+// coffee.forEach((item) => {
+//     const dom = renderTable(item);
+//     tableBody.appendChild(dom);
+// });
---
+++ shopping-cart/shopping-cart.js
@@ -0,0 +1,9 @@
+import cart from '../data/cart.js';
+import renderTable from './render-line-items.js';
+
+const tableBody = document.getElementById('table-generate');
+
+cart.forEach((item) => {
+    const dom = renderTable(item);
+    tableBody.appendChild(dom);
+});
