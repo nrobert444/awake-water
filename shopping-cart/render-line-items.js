ca9ff144cd0ba1274cfed8c00d9cab513d57c20b
Nicholas Roberto <njr444@gmail.com>
2020-01-23 08:25:06 -0800

    calclineitem and findbyid functions created and tested.

---
+++ shopping-cart/render-line-items.js
@@ -0,0 +1,38 @@
+<thead>
+    <tr>
+      <th>Drink</th>
+      <th>Qty</th> 
+      <th>Price</th>
+      <th>Total</th>
+</thead>
+<tbody>
+    <tr>
+      <td>americano</td>
+      <td>1</td>
+      <td>$3.00</td>
+      <td>$3.00</td>
+    </tr>
+    <tr>
+      <td>pour-over</td>
+      <td>3</td>
+      <td>$3.00</td>
+      <td>$9.00</td>
+    </tr>
+    <tr>
+      <td>macchiato</td>
+      <td>2</td>
+      <td>$4.00</td>
+      <td>$8.00</td>
+    </tr>
+</tbody>
+<tfoot>
+    <tr>
+      <td>Order Total</td>
+      <td></td>
+      <td></td>
+      <td>$20.00</td>
+    </tr>
+</tfoot>
+<table>
+<button>Place Order</button>
+ 
