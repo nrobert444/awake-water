e90816880f3655a44e768b2a46ba88ad798d2c97
Nicholas Roberto <njr444@gmail.com>
2020-01-23 12:54:09 -0800

    bug fixing

--- shopping-cart/render-line-items.js
+++ shopping-cart/render-line-items.js
@@ -1,5 +1,5 @@
-import coffee from './data/coffee.js';
-import { findById, calcLineItem, calcOrderTotal } from './common/utils.js';
+import coffee from '../data/coffee.js';
+import { findById, calcLineItem, calcOrderTotal } from '../common/utils.js';
 
 
 function renderTable(cartItem) {
@@ -9,6 +9,10 @@ function renderTable(cartItem) {
     const tabData = document.createElement('td');
     tabData.textContent = item.name;
     tabRow.appendChild(tabData);
+
+    const tabQuantity = document.createElement('td');
+    tabQuantity.textContent = cartItem.quantity;
+    tabRow.appendChild(tabQuantity);
 
     const tablePrice = document.createElement('td');
     tablePrice.textContent = item.price;
@@ -22,7 +26,7 @@ function renderTable(cartItem) {
     tabOrderTotal.textContent = calcOrderTotal(cart, coffee);
 
     return tabRow;
-}
+};
 
 export default renderTable;
 
